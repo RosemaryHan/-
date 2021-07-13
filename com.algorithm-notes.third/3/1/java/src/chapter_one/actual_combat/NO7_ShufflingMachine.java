7399a7f2bda046351e2c77cd24abeeca8995db9b
RosemaryHan <190363919@qq.com>
2021-07-13 23:54:33 +0800

    第三章-3.1，部分练习题

--- com.algorithm-notes.third/3/1/java/src/chapter_one/actual_combat/NO7_ShufflingMachine.java
+++ com.algorithm-notes.third/3/1/java/src/chapter_one/actual_combat/NO7_ShufflingMachine.java
@@ -1,5 +1,9 @@
 package actual_combat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -88,6 +92,53 @@ public class NO7_ShufflingMachine {
         for (int i = 0; i < 54; i++) {
             rules[i] = scanner.nextInt();
         }
+    }
 
+    public static void main_2(String[] args) throws IOException {
+        int n = 54;
+        char[] ch = { 'S', 'H', 'C', 'D' };
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j <= 13; j++) {
+                StringBuilder bd = new StringBuilder();
+                bd.append(ch[i]);
+                bd.append(j);
+                list.add(bd.toString());
+            }
+            if (i == 3) {
+                list.add("J1");
+                list.add("J2");
+            }
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int[] num = new int[60];
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(s[i]);
+        }
+        int[] start = new int[55];
+        int[] end = new int[55];
+        for (int i = 0; i < n; i++) {
+            start[i] = i;
+        }
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                end[num[j] - 1] = start[j];
+            }
+            for (int j = 0; j < n; j++) {
+                start[j] = end[j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                System.out.print(" ");
+            }
+            System.out.print(list.get(start[i]));
+
+        }
+        System.out.println();
     }
 }
